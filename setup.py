d21a80ce3cfae58917aa24f40f760b750873e671
Yhoorneman <Yhoorneman@schubergphilis.com>
2024-09-12 15:41:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.9',
         ],
     test_suite='tests',
     tests_require=test_requirements
